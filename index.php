401bf8d0e4b245de101a64504c669cc8d0f22ba7
EndriuK <endrit.kaskija@gmail.com>
2024-09-06 17:25:14 +0200

    [UPD] fine milestone uno

--- index.php
+++ index.php
@@ -2,47 +2,59 @@
 
 function randomPassword($length)
 {
-
-    $chars = "a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 0 1 2 3 4 5 6 7 8 9 ! @ # $ % & * _";
-    $charsArray = explode(" ", $chars);
+    $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*_";
     $password = '';
 
-
     for ($i = 0; $i < $length; $i++) {
-        $password .= $charsArray[mt_rand(0, count($charsArray) - 1)];
+        $password .= $chars[mt_rand(0, strlen($chars) - 1)];
     }
 
     return $password;
 }
 
+$generatedPassword = '';
 
+if (isset($_GET['input_string'])) {
+    $inputString = $_GET['input_string'];
+    $length = strlen($inputString);
 
+    if ($length > 0) {
+        $generatedPassword = randomPassword($length);
+    } else {
+        $generatedPassword = "Inserisci una stringa valida.";
+    }
+}
 
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="it">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Document</title>
+    <title>Generatore di Password</title>
 </head>
 
 <body>
-    <form action="./index.php" method="get">
-        <div class="mb-3">
-            <label class="form-label">inserisci un numero da 6 a 25</label>
-            <input type="number" min="6" max="25" class="form-control" name="length">
+    <div class="container mt-5">
+        <h1>Generatore di Password Casuali</h1>
+        <form action="index.php" method="get">
+            <div class="mb-3">
+                <label for="input_string" class="form-label">Inserisci una stringa (A-Z, a-z, 0-9, caratteri speciali)</label>
+                <input type="text" class="form-control" id="input_string" name="input_string" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Genera</button>
+        </form>
 
-        </div>
-        <div class="mb-3">
-            <label class="form-label">Password generata:</label>
-            <input type="password" class="form-control">
-        </div>
-        <button type="submit" class="btn btn-primary">Genera</button>
-    </form>
+        <?php if ($generatedPassword): ?>
+            <div class="mt-4">
+                <label class="form-label">Password generata:</label>
+                <input type="text" class="form-control" value="<?= htmlspecialchars($generatedPassword) ?>" readonly>
+            </div>
+        <?php endif; ?>
+    </div>
 </body>
 
 </html>
