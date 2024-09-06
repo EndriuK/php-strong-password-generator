448867305a7cdd3d5c54841411cc7d4756b4887f
EndriuK <endrit.kaskija@gmail.com>
2024-09-06 16:05:11 +0200

    [UPD] function random password

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php
 
+function randomPassword($length)
+{
+
+    $chars = "a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 0 1 2 3 4 5 6 7 8 9 ! @ # $ % & * _";
+    $charsArray = explode(" ", $chars);
+    $password = '';
+
+
+    for ($i = 0; $i < $length; $i++) {
+        $password .= $charsArray[mt_rand(0, count($charsArray) - 1)];
+    }
+
+    return $password;
+}
+
+
+
+
 ?>
 
 <!DOCTYPE html>
@@ -16,16 +34,12 @@
     <form action="./index.php" method="get">
         <div class="mb-3">
             <label class="form-label">inserisci un numero da 6 a 25</label>
-            <input type="number" min="6" max="25" class="form-control">
+            <input type="number" min="6" max="25" class="form-control" name="length">
 
         </div>
         <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password generata:</label>
+            <label class="form-label">Password generata:</label>
             <input type="password" class="form-control">
-        </div>
-        <div class="mb-3 form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
         </div>
         <button type="submit" class="btn btn-primary">Genera</button>
     </form>
